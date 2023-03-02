335e91b8bd88d0a60baa8ba9587899e58f5847e5
TomaszCzornak <tomasz.czornak@outlook.com>
2023-03-02 17:26:36 +0100

    optimized imports

--- src/main/java/com/packt/cardatabase/web/LoginController.java
+++ src/main/java/com/packt/cardatabase/web/LoginController.java
@@ -1,8 +1,9 @@
 package com.packt.cardatabase.web;
 
+import com.packt.cardatabase.domain.AccountCredentials;
+import com.packt.cardatabase.service.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -11,9 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.packt.cardatabase.domain.AccountCredentials;
-import com.packt.cardatabase.service.JwtService;
 
 @RestController
 public class LoginController {
